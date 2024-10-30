1f3db12bcc1fe824f06680169876decb06eb89fc
Sivuyile Shasha <sivuyile.shasha@vml.com>
2024-10-30 23:34:47 +0200

    conditoinal statement option 1

--- js/react_app/src/components/CallToAction.js
+++ js/react_app/src/components/CallToAction.js
@@ -6,10 +6,17 @@ export default () => {
   const styles  = 'btn btn-primary me-2';
   return (
     <>
-      <button onClick={()=>{setCount(count+1)}} type="button" className={styles} data-mdb-ripple-init>
-        Count is { count }!
+      <button onClick={()=>{setCount(count+1)}} type="button" className={`${styles}`} data-mdb-ripple-init>
+        {(() => {
+          if (count === 0) {
+            return "Click to start counting!";
+          }
+          else {
+            return `Current count is ${count}`;
+          }
+        })()}
       </button>
-      <button onClick={()=>{setCount(0)}} type="button" className={styles + ' btn-danger'} data-mdb-ripple-init>
+      <button onClick={()=>{setCount(0)}} type="button" className={`${styles} btn-danger`} data-mdb-ripple-init disabled={count === 0}>
         Reset
       </button>
     </>
