498fba861a5c41a046dd75c4077bcfa5ca18042d
Sivuyile Shasha <sivuyile.shasha@vml.com>
2024-10-30 13:50:02 +0200

    Configure builds.

---
+++ js/react_app/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const config = {
+  entry: './src/App.js',
+  devtool: (process.env.NODE_ENV === 'production') ? false : 'inline-source-map',
+  mode: (process.env.NODE_ENV === 'production')  ? 'production' : 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+};
+
+module.exports = config;
