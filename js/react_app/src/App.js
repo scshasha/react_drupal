d56006c68159bfa0e1675a54d4fe553b30cd14db
Sivuyile Shasha <sivuyile.shasha@vml.com>
2024-10-31 18:42:59 +0200

    Implement a load more button for item listing

--- js/react_app/src/App.js
+++ js/react_app/src/App.js
@@ -50,7 +50,7 @@ function App() {
       }
       {/*// Search form*/}
       <div className="row g-0 mb-3">
-        <div className="col-12">
+        <div className="col-12 mb-3">
           <input
             type="text"
             className="form-control w-100"
--- js/react_app/src/components/List.js
+++ js/react_app/src/components/List.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 
 export default ({ onChoice, searchTerm }) => {
   const [products, setProducts] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(6);
 
   async function fetchData() {
     try {
-      // Use the searchTerm in the API call
       const response = await fetch(`https://dummyjson.com/products/search?q=${searchTerm}`);
       const data = await response.json();
       setProducts(data.products);
@@ -14,24 +14,49 @@ export default ({ onChoice, searchTerm }) => {
     }
   }
 
-  // Call fetchData whenever searchTerm changes
   useEffect(() => {
     fetchData();
-  }, [searchTerm]); // Dependency array includes searchTerm
+  }, [searchTerm]);
+
+  const loadMore = () => {
+    setVisibleCount(prevCount => prevCount + 6);
+  };
 
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4">
-      {
-        products.map(item => (
-          <div className="col" key={item.id}> {/* Moved key here for the outer div */}
-            <div className="card">
-              <a onClick={() => { onChoice(item) }} className="data-mdb-tooltip-init" title={item.title}>
-                <img src={item.thumbnail} className="card-img-top" alt={item.title} />
-              </a>
+    <div className="col-md-12">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {
+          products.slice(0, visibleCount).map(item => (
+            <div className="col" key={item.id}>
+              <div className="card">
+                <a onClick={() => onChoice(item)} className="data-mdb-tooltip-init" title={item.title}>
+                  <img
+                    src={item.thumbnail}
+                    alt={item.title}
+                    className="card-img-top"
+                  />
+                </a>
+              </div>
             </div>
-          </div>
-        ))
-      }
+          ))
+        }
+      </div>
+
+      {visibleCount < products.length && (
+        <div className="text-center mt-3">
+          <button
+            onClick={loadMore}
+            className="btn btn-lg"
+            style={{
+              color: '#eaeaea',
+              background: '#4c7f8b',
+              cursor: 'pointer',
+            }}
+          >
+            Load More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
